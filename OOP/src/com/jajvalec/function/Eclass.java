20bd57adbda960d8825832f39ff09614ab6a39e2
gim970613 <gim970613@gamil.com>
2021-09-09 00:19:12 +0900

    Eclass ㅗ안성

--- OOP/src/com/jajvalec/function/Eclass.java
+++ OOP/src/com/jajvalec/function/Eclass.java
@@ -1,5 +1,14 @@
 package com.jajvalec.function;
 
 public class Eclass {
+	int one=0,tow=0;
 
+	public Eclass(int one, int tow) {
+		super();
+		this.one = one;
+		this.tow = tow;
+	}
+	public void EEE() {
+		System.out.println(one+"+"+tow+"="+(one+tow));
+	}
 }
